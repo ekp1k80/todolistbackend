c491bb2ef736bc10142fafcb6d7221b1dee003bc
federicog08 <fedegas08@gmail.com>
2024-12-21 01:50:43 -0300

    remove middleware

--- src/middleware/authMiddleware/index.ts
+++ src/middleware/authMiddleware/index.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-  if (!req.oidc.isAuthenticated() && process.env.NODE_ENV !== "test") {
-    res.status(401).json({ error: "Unauthorized" });
-    return;
-  }
+  // if (!req.oidc.isAuthenticated() && process.env.NODE_ENV !== "test") {
+  //   res.status(401).json({ error: "Unauthorized" });
+  //   return;
+  // }
 
   next();
 };
